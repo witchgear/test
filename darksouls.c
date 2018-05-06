13daf5651a393e9bf917f49a2a497429916ffd63
Hana Cho <hanacho252@gmail.com>
2018-05-06 16:43:13 -0700

    Initial Commit

---
+++ darksouls.c
@@ -0,0 +1,12 @@
+#include "computer.h"
+
+int main(int argc, char *argv[])
+{
+	//this line of Program Code will create the Video Game known as "Dark Souls"
+	//the value of this "Variable" is set to the integer "1", indicating "Dark Souls 1"
+	uint32_t darksouls = 1;
+	
+	//this line of the Program Code will run the Video Game known as "Dark Souls"
+	//when you run this program on your "Computer" you will be playing the Video Game: "Dark Souls"
+	computer.run(darksouls);
+}
