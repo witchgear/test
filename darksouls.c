a646a4df4b6df6d5e8bd4c17672cd7b54941f12b
Hana Cho <hanacho252@gmail.com>
2018-05-06 16:49:00 -0700

    The Video Game Will Run Good Now

--- darksouls.c
+++ darksouls.c
@@ -6,7 +6,12 @@ int main(int argc, char *argv[])
 	//the value of this "Variable" is set to the integer "1", indicating "Dark Souls 1"
 	uint32_t darksouls = 1;
 	
+	//this line of Program Code will allow the Video Game known as "Dark Souls" to run Good
+	//Good meaning that the Video Game will perform At Upmost Goodness on your Computer
+	bool good = true;
+	
 	//this line of the Program Code will run the Video Game known as "Dark Souls"
 	//when you run this program on your "Computer" you will be playing the Video Game: "Dark Souls"
-	computer.run(darksouls);
+	//Thanks to the good variable, the Video Game will be running with Good performance
+	computer.run(darksouls, good);
 }
