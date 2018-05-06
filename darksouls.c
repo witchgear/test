f95b7156dec59e8c1c18c5f696ee3e80153e7215
Hana Cho <hanacho252@gmail.com>
2018-05-06 16:56:31 -0700

    dark souls 2 is better you dummyhead

--- darksouls.c
+++ darksouls.c
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[])
 {
 	//this line of Program Code will create the Video Game known as "Dark Souls"
-	//the value of this "Variable" is set to the integer "1", indicating "Dark Souls 1"
-	uint32_t darksouls = 1;
+	//the value of this "Variable" is set to the integer "2", indicating "Dark Souls 2"
+	uint32_t darksouls = 2;
 	
 	//this line of Program Code will allow the Video Game known as "Dark Souls" to run Good
 	//Good meaning that the Video Game will perform At Upmost Goodness on your Computer
